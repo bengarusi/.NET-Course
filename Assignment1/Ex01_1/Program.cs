f5ba15441e3366f80ada5f5e1f319b3cad2c87ed
bengarusi <bgarusi24@gmail.com>
2026-04-03 17:57:36 +0300

    Add Assignment1 solution with Ex01 projects

---
+++ Assignment1/Ex01_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex01_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ex01_1 is working!");
+        }
+    }
+}
