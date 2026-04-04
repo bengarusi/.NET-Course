fa0545de077786b7a35b34900564e47be2c6c409
bengarusi <bgarusi24@gmail.com>
2026-04-04 18:09:13 +0300

    Resolved conflict - kept Ex01_1 from teammate

--- Assignment1/Ex01_1/Program.cs
+++ Assignment1/Ex01_1/Program.cs
@@ -1,32 +1,14 @@
 ﻿using System;
-
-
 namespace Ex01_1
 {
-   public class Program
+    internal class Program
     {
-        public static void Main(string[] args)
-        {   string[] binaryNumbers = new String[Globals.NumberOfBinaryNumbers];
-            int[] numbers = new int[Globals.NumberOfBinaryNumbers];
-            for (int inputNumbers = 0; inputNumbers < Globals.NumberOfBinaryNumbers; inputNumbers++)
-            {
-               Console.WriteLine("Enter a 7 digit binary number");
-               string binaryNumber = Console.ReadLine();
-               if(!Validation.InputValidateion(binaryNumber))
-                {
-                    inputNumbers--;
-                }
-               else
-                {
-                    binaryNumbers[inputNumbers] = binaryNumber;
-                }
-            }
-            for(int i=0; i < Globals.NumberOfBinaryNumbers; i++)
-            {
-                numbers[i] = BinaryToDemicalConverter.ConvertBinaryToDecimal(binaryNumbers[i]);
-            }
-           Statistics.PrintStatistics(binaryNumbers,numbers);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("" + "Ex01_1 is working!");
 
+     
         }
+        class A { public void Test() { int x = 5; } }
     }
 }
