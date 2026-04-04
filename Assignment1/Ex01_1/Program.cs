f5645f01b5dd9960411ebe61ec8b429bdd1dc31c
gallibal <gallibal18@gmail.com>
2026-04-04 18:45:51 +0300

    Ex01_1 v3

--- Assignment1/Ex01_1/Program.cs
+++ Assignment1/Ex01_1/Program.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+
+
 namespace Ex01_1
 {
-    internal class Program
+   public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("" + "Ex01_1 is working!");
-
-     
+            string[] binaryNumbers = new String[Globals.NumberOfBinaryNumbers];
+            int[] numbers = new int[Globals.NumberOfBinaryNumbers];
+            for (int inputNumbers = 0; inputNumbers < Globals.NumberOfBinaryNumbers; inputNumbers++)
+            {
+                Console.WriteLine("Enter a 7 digit binary number");
+                string binaryNumber = Console.ReadLine();
+                if (!Validation.InputValidateion(binaryNumber))
+                {
+                    inputNumbers--;
+                }
+                else
+                {
+                    binaryNumbers[inputNumbers] = binaryNumber;
+                }
+            }
+            for (int i = 0; i < Globals.NumberOfBinaryNumbers; i++)
+            {
+                numbers[i] = BinaryToDemicalConverter.ConvertBinaryToDecimal(binaryNumbers[i]);
+            }
+            Statistics.PrintStatistics(binaryNumbers, numbers);
         }
-        class A { public void Test() { int x = 5; } }
     }
 }
